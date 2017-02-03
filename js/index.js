90af30c418d769b69f93563a644cdefd83b0d5ed
ldevernay <boblehamster@yahoo.fr>
2017-02-03 09:37:38 +0100

    game over

--- js/index.js
+++ js/index.js
@@ -25,18 +25,20 @@ $(document).ready(function(){
     nbMyst = (Math.floor((9)*Math.random()+1));
     Chronometre.start();
     $("#start").hide();
+    $("#over").hide();
     $("#guessInput").show();
     $("#guessBtn").show();
+    i = 1;
   });
 
   $( "#guessForm" ).submit(function( event ) {
-  $("#next").hide();
+    $("#next").hide();
     guessMysteryNumber();
   });
 
   $( "#guessBtn" ).click(function() {
-  $( "#guessForm" ).submit();
-});
+    $( "#guessForm" ).submit();
+  });
 
   function timer(){
     Chronometre.time -=1;
@@ -61,29 +63,38 @@ $(document).ready(function(){
     $("#timer").html("01:00");
   }
 
+  function gameOver(){
+    $("#over").html('GAME OVER');
+    $("#over").show();
+    $("#start").show();
+    $("#result").empty();
+    $("#guessInput").val('');
+    $("#guessInput").hide();
+    $("#guessBtn").hide();
+    stop();
+  }
+
   function guessMysteryNumber(){
     var guess = document.getElementById("guessInput").value;
 
-      console.log(i);
-      if (guess < nbMyst){
-        $("#result").html('Le nombre mystère est plus grand !');
-      } if (guess > nbMyst) {
-        $("#result").html('Le nombre mystère est plus petit !');
-        // var essai = prompt('Devinez le nombre mystère, il est compris entre 0 et 9');
-      } if (guess == nbMyst) {
-        $("#result").html('Bravo ! le nombre mystère était bien ' + nbMyst);
-        $("#next").html('Nouveau chiffre!');
-        nbMyst = (Math.floor((9)*Math.random()+1));
-        i = 0;
-      }
-      i++;
+    console.log(nbMyst);
+    if (guess < nbMyst){
+      $("#result").html('Le nombre mystère est plus grand !');
+    } if (guess > nbMyst) {
+      $("#result").html('Le nombre mystère est plus petit !');
+      // var essai = prompt('Devinez le nombre mystère, il est compris entre 0 et 9');
+    } if (guess == nbMyst) {
+      $("#result").html('Bravo ! le nombre mystère était bien ' + nbMyst);
+      $("#next").html('Nouveau chiffre!');
+      $("#next").show();
+      nbMyst = (Math.floor((9)*Math.random()+1));
+      i = 0;
+    }
+    i++;
 
-      if (i>3){
-        $("#over").html('GAME OVER');
-        $("#start").show();
-        $("#guessInput").hide();
-        $("#guessBtn").hide();
-      }
+    if (i>3){
+      gameOver();
+    }
   }
 
 });
